e083521bfc6b5fe5e53894dca73a00b41ae96cd4
Bruno Miguel Alves Costa <brunomiguelalvescosta@brunos-air.kpn>
2022-02-28 22:52:51 +0100

    First commit: add functions for subtraction, division, multiplication and addition

---
+++ assignment.js
@@ -0,0 +1,35 @@
+const subtraction = () => {
+  for (let num = 1; num < 11; num++) {
+    const subtractor = 3;
+    const formula = `${num} - ${subtractor}`;
+    const subtractionResult = `${num}` - `${subtractor}`;
+    console.log(formula + " = " + subtractionResult);
+  }
+};
+
+const division = () => {
+  for (let num = 1; num < 11; num++) {
+    const divisor = 3;
+    const formula = `${num} / ${divisor}`;
+    const divisionResult = `${num}` / `${divisor}`;
+    console.log(formula + " = " + divisionResult);
+  }
+};
+
+const multiplication = () => {
+  for (let num = 1; num < 11; num++) {
+    const multiplier = 3;
+    const formula = `${num} * ${multiplier}`;
+    const multiplicationResult = `${num}` * `${multiplier}`;
+    console.log(formula + "\n = " + multiplicationResult);
+  }
+};
+
+const addition = () => {
+    for (let num = 1; num < 11; num++) {
+        const addend = 3;
+        const formula = `${num} + ${addend}`;
+        const additionResult = `${num}` + `${addend}`;
+        console.log(formula + ' = ' + additionResult);
+    }
+}
